942c53ecfb5ba884b08c76cec22875a7fbf75bf6
Timur <10380147+ShadyBytes@users.noreply.github.com>
2022-09-14 17:34:25 +0300

    chess king impl

---
+++ tasks/05_chess_king/main.py
@@ -0,0 +1,15 @@
+# Author: Timur Bagirov
+# Date: 14 September 2022
+# Шахматный король
+
+
+def main():
+    x1, y1, x2, y2 = int(input()), int(input()), int(input()), int(input())
+    if x2 - x1 <= 1 and y2 - y1 <= 1:
+        print("YES")
+    else:
+        print("NO")
+
+
+if __name__ == "__main__":
+    main()
