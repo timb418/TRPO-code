ed65c2b71ba0aa0d4a675a06c3d05e30f516c8df
Timur <10380147+ShadyBytes@users.noreply.github.com>
2022-09-15 21:54:08 +0300

    chess fix + mkad impl

--- tasks/05_chess_king/main.py
+++ tasks/05_chess_king/main.py
@@ -5,7 +5,7 @@
 
 def main():
     x1, y1, x2, y2 = int(input()), int(input()), int(input()), int(input())
-    if x2 - x1 <= 1 and y2 - y1 <= 1:
+    if abs(x2 - x1) <= 1 and abs(y2 - y1) <= 1:
         print("YES")
     else:
         print("NO")
--- tasks/07_circular_road/main.py
+++ tasks/07_circular_road/main.py
@@ -3,7 +3,13 @@
 # Кольцевая автомобильная дорога
 
 def main():
-    pass
+    mkad_length = 109
+    v = int(input())
+    t = int(input())
+    if v >= 0:
+        print(v * t % mkad_length)
+    else:
+        print("-{0}".format(v * t % mkad_length))
 
 
 if __name__ == "__main__":
