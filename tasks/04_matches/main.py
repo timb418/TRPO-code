ec2a483ebc98fe14ccefad48891ee43e763d438c
Timur <10380147+ShadyBytes@users.noreply.github.com>
2022-09-16 18:18:31 +0300

    matches fix

--- tasks/04_matches/main.py
+++ tasks/04_matches/main.py
@@ -3,25 +3,43 @@
 # Спички
 
 
+class Match:
+
+    @classmethod
+    def from_coords(cls, coords):
+        yield cls(coords[0:2])
+        yield cls(coords[2:4])
+        yield cls(coords[4:6])
+
+    def __init__(self, endpoints):
+        self.x1, self.x2 = sorted(endpoints)
+
+    def __invert__(self):
+        return self.x2 - self.x1
+
+    def __xor__(m1, m2):
+        return max(m1.x2, m2.x2) - min(m1.x1, m2.x1) - ~m1 - ~m2
+
+    def __and__(m1, m2):
+        return m1 ^ m2 <= 0
+
+
+def solve(*coords):
+    m1, m2, m3 = Match.from_coords(coords)
+    if (m1 & m2) + (m1 & m3) + (m2 & m3) >= 2:
+        return 0
+    if ~m1 >= m2 ^ m3:
+        return 1
+    if ~m2 >= m1 ^ m3:
+        return 2
+    if ~m3 >= m1 ^ m2:
+        return 3
+    return -1
+
+
 def main():
     l1, r1, l2, r2, l3, r3 = int(input()), int(input()), int(input()), int(input()), int(input()), int(input())
-
-    if (l1 >= 0) and (l1 < r1) and (r1 <= 100) and \
-            (l2 >= 0) and (l2 < r2) and (r2 <= 100) and \
-            (l3 >= 0) and (l3 < r3) and (r3 <= 100):
-        if ((l2 - r1) <= 1) and ((l3 - r2) > 1) and (l1 < l2) and \
-                (l2 < r3) and (l1 < r3):
-            print(3)
-        elif ((l2 - r1) > 1) and ((l3 - r2) > 1) and (l2 - r1) <= (r3 - l3):
-            print(3)
-        elif ((l2 - r1) > 1) and ((l3 - r2) <= 1):
-            print(1)
-        elif ((l2 - r1) <= 1) and (((l3 - r2) <= 1) or ((l3 - r1) <= 1)):
-            print(0)
-        else:
-            print(-1)
-    else:
-        print(-1)
+    print(solve(l1, r1, l2, r2, l3, r3))
 
 
 if __name__ == "__main__":
