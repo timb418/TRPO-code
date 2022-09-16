ec9418e7caf4e30c492815c7285ef67724304f27
Timur <10380147+ShadyBytes@users.noreply.github.com>
2022-09-16 18:02:17 +0300

    algorithm fix

--- tasks/03_simple_equation/main.py
+++ tasks/03_simple_equation/main.py
@@ -5,12 +5,13 @@
 
 def main():
     a, b, c, d = int(input()), int(input()), int(input()), int(input())
-    if a == c and b == d:
-        print("NO")
-    elif a == 0 and b == 0:
-        print("INF")
+
+    if a == 0 and b == 0:
+        print('INF')
+    elif a == 0 or b * c == a * d:
+        print('NO')
     elif b % a == 0:
-        x = -b / a
+        x = -b // a
         print(x)
     else:
         print('NO')
